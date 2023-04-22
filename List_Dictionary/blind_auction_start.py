f778487fbd70aa1f88dff2782e8077a0b43f8f3d
Your Name <you@example.com>
2023-04-22 21:15:57 +0100

    Solved it!

--- List_Dictionary/blind_auction_start.py
+++ List_Dictionary/blind_auction_start.py
@@ -1,4 +1,4 @@
-from replit import clear
+#from replit import clear
 #HINT: You can call clear() to clear the output in the console.
 from art import logo
 
@@ -7,18 +7,29 @@ final_choice = True
 data_dictionary = {}
 bid_choice = "yes"
 data_host = []
-i = 0
+num = 0
+name = ''
+bid_winner = {}
+winner = []
 while bid_choice == "yes":
   #data_dictionary{}
   input_name = input("What is your name?: ")
-  input_bid = input("What's your bid?: $")
+  input_bid = int(input("What's your bid?: $"))
 
   data_dictionary["name"] = input_name
   data_dictionary["bid"] = input_bid
+  
+  if input_bid > num:
+      bid_winner["name"] = input_name
+      bid_winner["bid"] = input_bid
+      num = input_bid
  
   print(data_dictionary)
   data_host.append(data_dictionary.copy())
   
   bid_choice = input("Are there any other bidders? Type 'yes' or 'no'. \n")
+  
   #clear()
-print(data_host)
+#print(data_host)
+print (bid_winner)
+
