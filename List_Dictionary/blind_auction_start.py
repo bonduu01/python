1a54b0266e40db4e829c597c60bce72f8342fc86
Your Name <you@example.com>
2023-04-23 09:06:45 +0100

    leap year

--- List_Dictionary/blind_auction_start.py
+++ List_Dictionary/blind_auction_start.py
@@ -3,33 +3,22 @@
 from art import logo
 
 print(logo)
-final_choice = True
-data_dictionary = {}
 bid_choice = "yes"
-data_host = []
-num = 0
+highest_bid = 0
 name = ''
 bid_winner = {}
-winner = []
 while bid_choice == "yes":
   #data_dictionary{}
   input_name = input("What is your name?: ")
   input_bid = int(input("What's your bid?: $"))
-
-  data_dictionary["name"] = input_name
-  data_dictionary["bid"] = input_bid
   
-  if input_bid > num:
+  if input_bid > highest_bid:
       bid_winner["name"] = input_name
       bid_winner["bid"] = input_bid
-      num = input_bid
- 
-  print(data_dictionary)
-  data_host.append(data_dictionary.copy())
+      highest_bid = input_bid
+      name = input_name
   
   bid_choice = input("Are there any other bidders? Type 'yes' or 'no'. \n")
   
-  #clear()
-#print(data_host)
-print (bid_winner)
+print (f"The winner is {name} with a bid of ${highest_bid}")
 
